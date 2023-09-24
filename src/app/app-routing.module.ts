dd68f648aa6587e4236d5a006073b2dcc96358b7
haozhenhua <1305510482@qq.com>
2023-09-24 17:23:00 +0900

    创建路由 并 探索 routerLinkActive

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { FirstComponent } from './first/first.component';
+import { SecondComponent } from './second/second.component';
+
+// 在 Routes 数组中定义您的路线。
+// 该数组中的每个路由都是一个包含两个属性的 JavaScript 对象。
+// 第一个属性 path 定义路由的 URL 路径。
+// 第二个属性 component 定义 Angular 应用于相应路径的组件。
+const routes: Routes = [
+  { path: 'first-component', component: FirstComponent },
+  { path: 'second-component', component: SecondComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
